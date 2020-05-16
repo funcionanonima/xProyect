93a926628b4be8ef0a870952a2f7b90040ab570e
Bambooh <cdgaviria40@misena.edu.co>
2020-05-15 21:53:10 -0500

    Proyecto de 0, vista App básica

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class xProject extends Component {
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.Text}>does it takes too much?</Text>
+      </View>    
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({  
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection:'column',
+  },
+  Text: {
+    color: '#fff',
   },
 });
+
+export default xProject;
